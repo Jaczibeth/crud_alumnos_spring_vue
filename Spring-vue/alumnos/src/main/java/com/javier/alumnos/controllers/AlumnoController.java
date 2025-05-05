bd492bd9804a743b480b71f27b151ca49a555104
javiern-778 <jcruzespana2@gmail.com>
2025-05-05 16:13:55 -0600

    Act5-Creando Metodo POST en el cotrolador

--- Spring-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
+++ Spring-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javier.alumnos.model.Alumno;
 import com.javier.alumnos.repository.AlumnoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/alumnos")
@@ -17,9 +20,17 @@ public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
 
+    //metodo get para traer to dos los alumnos de la base de datos
     @GetMapping("/traer-alumnos")
     public List<Alumno> TraerAlumno() {
         return alumnoRepository.findAll();
     }
-
+    //metodo para insertar un alumno en la base de datos
+    @PostMapping("/insertar-alumnos")
+    public Alumno insertarAlumno(@RequestBody Alumno alumno) {
+        return alumnoRepository.save(alumno);
+        
+    
+    }
+    
 }
