dfb4325ea63a9f9c5c19314b7dd2271e103e73be
javiern-778 <jcruzespana2@gmail.com>
2025-05-05 17:06:52 -0600

    Act6-Creando Metodo PUT en el controlador

--- Spring-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
+++ Spring-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
@@ -3,14 +3,17 @@ package com.javier.alumnos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javier.alumnos.model.Alumno;
 import com.javier.alumnos.repository.AlumnoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -32,5 +35,19 @@ public class AlumnoController {
         
     
     }
-    
+    //Metodo para editar un alumno en la base de datos
+    @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<Alumno> ActualizarAlumno(@PathVariable Long id, @RequestBody Alumno alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellidos(alumno.getApellidos());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setEmail(alumno.getEmail());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setImagenURL(alumno.getImagenURL());
+            Alumno actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
+    }
 }
