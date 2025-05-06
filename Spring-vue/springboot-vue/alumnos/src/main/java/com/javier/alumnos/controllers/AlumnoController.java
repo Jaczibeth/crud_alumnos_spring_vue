b7de35483e76bbd09b64abc0c060da9618c35ba1
javiern-778 <jcruzespana2@gmail.com>
2025-05-05 19:28:21 -0600

    Act13- Dando Permisos de CrossOrigin

--- Spring-vue/springboot-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
+++ Spring-vue/springboot-vue/alumnos/src/main/java/com/javier/alumnos/controllers/AlumnoController.java
@@ -16,10 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
 @RequestMapping("/alumnos")
+@CrossOrigin(origins = "*")
 public class AlumnoController {
 
     @Autowired
